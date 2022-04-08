1fd3e50017fbc7bbefa6e45d4eddc54f5dc94193
Pedro Feltrin <pedrofeltrin45@gmail.com>
2022-04-08 09:11:04 -0300

    trying out some String methods

--- section-2/hello-world.rb
+++ section-2/hello-world.rb
@@ -1,1 +1,10 @@
-puts "Hello, World"
+puts "What's your first name?"
+name = gets
+text = "Hello, #{name.rstrip}, nice to meet you!"
+puts text
+text["Hello"] = "Bye"
+puts text
+
+# methods
+puts text.nil?
+puts text.empty?
