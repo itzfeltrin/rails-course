16446999fc30b0abdbc2fbea864e75bf25b22617
Pedro Feltrin <pedrofeltrin45@gmail.com>
2022-04-09 23:14:04 -0300

    first assignment: String methods

--- section-2/hello-world.rb
+++ section-2/hello-world.rb
@@ -1,6 +1,7 @@
+=begin
 puts "What's your first name?"
-name = gets
-text = "Hello, #{name.rstrip}, nice to meet you!"
+name = gets.chomp
+text = "Hello, #{name}, nice to meet you!"
 puts text
 text["Hello"] = "Bye"
 puts text
@@ -8,3 +9,13 @@ puts text
 # methods
 puts text.nil?
 puts text.empty?
+=end
+
+puts "What is your first name?"
+first_name = gets.chomp
+puts "What is your last name?"
+last_name = gets.chomp
+full_name = "#{first_name} #{last_name}"
+puts "Your full name is #{full_name}"
+puts "Your full name reversed is #{full_name.reverse}"
+puts "Your name has #{full_name.gsub(/\s+/, "").length} characters in it"
