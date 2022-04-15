78264d5823896902f668533a98f393cc55e45a10
Pedro Feltrin <pedrofeltrin45@gmail.com>
2022-04-15 17:46:25 -0300

    learn some more about OOP accessors in ruby

--- section-2/oop/student.rb
+++ section-2/oop/student.rb
@@ -1,16 +1,27 @@
 class Student
-  @first_name
-  @last_name
-  @email
-  @username
-  @password
+  attr_accessor :first_name, :last_name, :email, :username, :password
+
+  def initialize(first_name, last_name, email, username, password)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @username = username
+    @password = password
+  end
 
   def to_s
-    "First name: #{@first_name}"
+    "First name: #{@first_name}, Last name: #{@last_name}, Username: #{@username}, Email address: #{@email}"
   end
 
 end
 
-pedro = Student.new
-pedro.first_name = "Pedro"
+pedro = Student.new("Pedro", "Feltrin", "pedrofeltrin45@gmail.com", "itzfeltrin",
+                    "abc123")
+john = Student.new("John", "Doe", "john1@example.com", "john1",
+                   "password2")
+
+puts pedro
+puts john
+pedro.last_name = john.last_name
+puts "Pedro is altered"
 puts pedro
