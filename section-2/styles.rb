ac9ea487ad12a76d406bdff14ace1aa00420c375
Pedro Feltrin <pedrofeltrin45@gmail.com>
2022-04-15 16:46:00 -0300

    add ruby style conventions file

---
+++ section-2/styles.rb
@@ -0,0 +1,15 @@
+# use snake case for variables, functions
+my_variable = 0
+
+def my_function
+  # ruby tab has 2 spaces
+end
+
+# use pascal case for classes
+class MyClass
+  attr_accessor name
+
+  def initialize(name)
+    @name = name
+  end
+end
