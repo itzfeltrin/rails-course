0001b6bd558bfcdea09872825c179beadb6bc962
Pedro Feltrin <pedrofeltrin45@gmail.com>
2022-04-26 11:32:07 -0300

    create new article page

--- section-3/alpha-blog/app/controllers/articles_controller.rb
+++ section-3/alpha-blog/app/controllers/articles_controller.rb
@@ -7,4 +7,13 @@ class ArticlesController < ApplicationController
     # binding.break - debug
     @article = Article.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    redirect_to @article
+  end
 end
