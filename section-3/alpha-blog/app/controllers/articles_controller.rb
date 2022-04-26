2ecff51e78b9b701e2eb8be3b9f37af7bd6715b8
Pedro Feltrin <pedrofeltrin45@gmail.com>
2022-04-26 13:35:43 -0300

    add link to show article and button to delete it

--- section-3/alpha-blog/app/controllers/articles_controller.rb
+++ section-3/alpha-blog/app/controllers/articles_controller.rb
@@ -35,4 +35,10 @@ class ArticlesController < ApplicationController
       render 'articles/edit'
     end
   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
 end
