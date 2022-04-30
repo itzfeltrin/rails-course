c49bba37973cd3ff306870a61ccc41b58f7db798
izfeltrin <pedrofeltrin45@gmail.com>
2022-04-30 10:51:12 -0300

    add validations to user model

--- section-6/alpha-blog/app/models/user.rb
+++ section-6/alpha-blog/app/models/user.rb
@@ -1,3 +1,19 @@
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z\d]+\.)+[a-z]{2,})\z/i
+      record.errors.add attribute, (options[:message] || "is not an email")
+    end
+  end
+end
+
 class User < ApplicationRecord
-  has_many :articles
+  validates :username,
+            presence: true,
+            length: { minimum: 3, maximum: 25 },
+            uniqueness: { case_sensitive: false }
+  validates :email,
+            presence: true,
+            email: true,
+            length: { maximum: 105 },
+            uniqueness: { case_sensitive: false }
 end
