1d1c7ec85d56a60036275e81577b4adedec8ce04
izfeltrin <pedrofeltrin45@gmail.com>
2022-04-30 23:21:10 -0300

    add user profile picture add articles to user page

--- section-6/alpha-blog/app/helpers/application_helper.rb
+++ section-6/alpha-blog/app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def gravatar_for(user, options = { size: 80 })
+    hash = Digest::MD5.hexdigest(user.email)
+    size = options[:size]
+    url = "https://gravatar.com/avatar/#{hash}?size=#{size}"
+    image_tag(url, alt: user.username, class: "rounded mx-auto d-block shadow")
+  end
 end
